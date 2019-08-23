4dc4629f2c93f520ac0f0dfc80dd7303e29cd821
Michael Kroon <kroonology@gmail.com>
2019-08-23 16:01:24 -0600

    add seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# use 'rails db:reset' from the command line to recreate/reset db
+
+# using faker gem
+# randomly generates bands and genres
+25.times do
+  Artist.create(
+    name: Faker::Music.band,
+    genre: Faker::Music.genre
+    )
+end
+
+# randomly generates billboard names from publisher titles...
+5.times do
+  Billboard.create(
+    name: Faker::Book.publisher
+  )
+end
+
+# randomly generates songs, and assigns them to an artist and billboard
+100.times do
+  Song.create(
+    title: Faker::Book.title,
+    artist_id: rand(100) + 1,
+    billboard_id: rand(5) +1
+  )
+end
